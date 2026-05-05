7a499546a216a9ceb4ba4fc4effe02506a684be1
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-05-05 15:33:52 +0300

    Implemented validation service.

--- src/main/java/ua/ivan/epam/gym/application/authentication/AuthService.java
+++ src/main/java/ua/ivan/epam/gym/application/authentication/AuthService.java
@@ -1,23 +1,20 @@
 package ua.ivan.epam.gym.application.authentication;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import ua.ivan.epam.gym.application.dao.UserDao;
 import ua.ivan.epam.gym.application.model.User;
+import ua.ivan.epam.gym.application.repository.UserRepository;
 
 @Component
+@RequiredArgsConstructor
 public class AuthService {
 
-    private final UserDao userDao;
-
-    @Autowired
-    public AuthService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    private final UserRepository userRepo;
 
     public void authenticate (String username, String password) {
-        User user = userDao.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
 
         if (!user.getPassword().equals(password)) {
             throw new RuntimeException("Invalid password");
