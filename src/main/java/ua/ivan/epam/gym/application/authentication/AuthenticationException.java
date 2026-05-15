feb47e8385f58852b6081d2de33f6293b27a5bfd
Ivan (Lain) <ivan.shankin04@gmail.com>
2026-05-15 17:50:55 +0300

    Add REST error handling

---
+++ src/main/java/ua/ivan/epam/gym/application/authentication/AuthenticationException.java
@@ -0,0 +1,8 @@
+package ua.ivan.epam.gym.application.authentication;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
