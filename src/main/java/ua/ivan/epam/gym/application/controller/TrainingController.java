3ee3f596937f479dffe240d2a616ef85eaf30f66
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-05-22 15:28:24 +0300

    Changed all Swagger v2 annotations in controllers to Swagger v3.

--- src/main/java/ua/ivan/epam/gym/application/controller/TrainingController.java
+++ src/main/java/ua/ivan/epam/gym/application/controller/TrainingController.java
@@ -1,9 +1,9 @@
 package ua.ivan.epam.gym.application.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +15,20 @@ import ua.ivan.epam.gym.application.service.TrainingService;
 @RestController
 @RequestMapping("/api/v1/trainings")
 @RequiredArgsConstructor
-@Api(tags = "Trainings")
+@Tag(name = "Trainings")
 public class TrainingController {
 
     private final TrainingService trainingService;
 
     @RequireAuth
     @PostMapping
-    @ApiOperation(value = "Add training")
+    @Operation(summary = "Add training")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully added training"),
-            @ApiResponse(code = 400, message = "Invalid request body or validation error"),
-            @ApiResponse(code = 401, message = "You are not authorized to create the resource"),
-            @ApiResponse(code = 404, message = "Trainee or trainer was not found"),
-            @ApiResponse(code = 500, message = "Application failed to process the request")
+            @ApiResponse(responseCode = "200", description = "Successfully added training"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body or validation error"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to create the resource"),
+            @ApiResponse(responseCode = "404", description = "Trainee or trainer was not found"),
+            @ApiResponse(responseCode = "500", description = "Application failed to process the request")
     })
     public ResponseEntity<Void> addTraining(@Valid @RequestBody AddTrainingRequest request) {
         trainingService.create(request);
