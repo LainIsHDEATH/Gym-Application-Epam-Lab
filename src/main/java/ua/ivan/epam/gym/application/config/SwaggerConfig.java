567664c2470cae10cc04c1ec4072542195f2c93c
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-06-07 18:49:11 +0300

    Added bearer token auth into swagger ui.

--- src/main/java/ua/ivan/epam/gym/application/config/SwaggerConfig.java
+++ src/main/java/ua/ivan/epam/gym/application/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package ua.ivan.epam.gym.application.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("API documentation for Gym application")
                         .description("Gym application API")
